98d7026d6c722fcf009fb9881e6e58c071bf5320
FuzzyKittens <kory.kliner@outlook.com>
2023-06-06 20:58:29 -0400

    Create notepad.ps1

---
+++ notepad.ps1
@@ -0,0 +1,20 @@
+function Send-Keystrokes {
+    [cmdletbinding()]
+    param
+    (
+        [Parameter(Mandatory=$true)]
+        [string]$String
+    )
+    foreach ($char in $string) {
+        $wsh.SendKeys($char)
+        Start-Sleep -Milliseconds 800
+    }
+}
+
+$wsh = New-Object -ComObject WScript.Shell
+$wsh.SendKeys('^{ESC}{+}')
+Start-Sleep -Second 1
+Send-Keystrokes -String "notepad"
+$wsh.SendKeys('{ENTER}')
+Start-Sleep -Second 1
+Send-Keystrokes "Please remember to lock your computer when you walk away."
